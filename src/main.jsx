e0b5c445863e707754c0281c1ad97f0f5c641479
utpalpatel0055 <utpalpatel0055@gmail.com>
2024-01-22 15:32:09 -0500

    Router added
    Going to update routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Home from './pages/home/Home.jsx'
+import {Route, createBrowserRouer, createRoutesFromElements} from 'react-router-dom'
+import About from './pages/about/About.jsx'
+
+const router = createBrowserRouer(
+  createRoutesFromElements(
+    <Route path='/' element={<Home/>}>
+      <Route path='about' element={<About/>} />
+    </Route>
+  )
+
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Home/>
+  </React.StrictMode>
 )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,18 @@
+import Footer from '../../components/footer/Footer';
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
 import './home.scss'
 
 const Home = () => {
     return (
+        
         <div className='home'>
-            Home
+            <Sidebar />
+            <div className='main'>
+                <Navbar />
+                Home
+                <Footer />
+            </div>
         </div>
     )
 }
